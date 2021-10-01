ac03857204092d52b0776830179bd748f725620c
smhrd <smhrd@DESKTOP-TAG4B6N>
2021-10-01 16:47:35 +0900

    테스트용

--- GitTest/src/main/java/com/smhrd/TEST01.java
+++ GitTest/src/main/java/com/smhrd/TEST01.java
@@ -5,8 +5,8 @@ public class TEST01 {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("여러분 만나서 반갑습니다 저는 마라탕 먹은후로 배가 아파와요");
-		System.out.println("갑자기 빨리 집에 가고싶습니다");
+		System.out.println("Hello everybody make some noise~~");
+		System.out.println("how are you~!");
 	}
 
 }
