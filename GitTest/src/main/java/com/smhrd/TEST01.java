7d00a881239efff5c5f4977bc956057f9aad7378
smhrd <smhrd@DESKTOP-TAG4B6N>
2021-10-01 16:41:28 +0900

    테스트 용도

---
+++ GitTest/src/main/java/com/smhrd/TEST01.java
@@ -0,0 +1,12 @@
+package com.smhrd;
+
+public class TEST01 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("여러분 만나서 반갑습니다 저는 마라탕 먹은후로 배가 아파와요");
+		System.out.println("갑자기 빨리 집에 가고싶습니다");
+	}
+
+}
