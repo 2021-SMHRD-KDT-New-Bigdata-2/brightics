770c01ec79fef1109abdde9a15d9929450dd4813
임재곤 <limjae0@gmail.com>
2021-10-07 09:37:54 +0900

    2021/10/06 - 구글 트랜드 연동

---
+++ src/main/java/com/brightics/prj/controller/MainApiController.java
@@ -0,0 +1,14 @@
+package com.brightics.prj.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainApiController {
+
+
+//    @GetMapping("candidate/getgraphinfo")
+//    public List<>
+
+}
